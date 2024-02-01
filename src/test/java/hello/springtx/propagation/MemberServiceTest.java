57217018f59a222e4eb2a03797e03cf6c108b459
Jongwook-Seong <ypwa121@gmail.com>
2024-02-01 15:12:22 +0900

    learn: 트랜잭션 전파 활용4 - 전파 커밋

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -25,6 +26,61 @@ class MemberServiceTest {
     void outerTxOff_success() {
         //given
         String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional: OFF
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "outerTxOff_fail";
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username))
+                .isInstanceOf(RuntimeException.class);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: OFF
+     * logRepository    @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "outerTxOff_success";
+
+        //when
+        memberService.joinV1(username);
+
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON
+     */
+    @Test
+    void singleTxOn_success() {
+        //given
+        String username = "outerTxOm_success";
 
         //when
         memberService.joinV1(username);
